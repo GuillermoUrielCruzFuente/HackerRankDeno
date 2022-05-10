97a0f4936609a2aa57f41b65c1e9c687595cf5a9
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-05-10 13:01:51 -0500

    Diagonal differences ready

---
+++ Week-02/DiagonalDifference.ts
@@ -0,0 +1,68 @@
+/**
+ * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+ */
+
+const diagonalDifference = (matrix: Array<Array<number>>): number => {
+    const primaryDiagonalIndices = getPrimaryDiagonalIndices(matrix[0].length)
+    const secondaryDiagonalIndices = getSecondaryDiagonalIndices(matrix[0].length)
+
+    let primarySum = 0
+    let secondarySum = 0
+
+    for (let i = 0; i < matrix[0].length; i++) {
+        primarySum += matrix[primaryDiagonalIndices[i].row][primaryDiagonalIndices[i].column]
+        secondarySum += matrix[secondaryDiagonalIndices[i].row][secondaryDiagonalIndices[i].column]
+    }
+
+    return Math.abs(primarySum - secondarySum)
+}
+
+type MatrixCoordinate = {
+    row: number,
+    column: number
+}
+/**
+ * The left-to-right diagonal indices in a square matrix
+ * @param squareMatrixSize 
+ * @returns Array of primary diagonal indices in square matrix
+ */
+const getPrimaryDiagonalIndices = (squareMatrixSize: number): Array<MatrixCoordinate> => {
+    let primaryDiagonal: Array<MatrixCoordinate> = []
+
+    for (let i = 0; i < squareMatrixSize; i++) {
+        primaryDiagonal.push({
+            row: i,
+            column: i
+        })
+    }
+
+    return primaryDiagonal
+}
+
+/**
+ * The right to left diagonal indices in a square matrix
+ * @param squareMatrixSize 
+ * @returns Array of secondary diagonal indices in square matrix
+ */
+const getSecondaryDiagonalIndices = (squareMatrixSize: number): Array<MatrixCoordinate> => {
+    let primaryDiagonal: Array<MatrixCoordinate> = []
+
+    for (let i = 0; i < squareMatrixSize; i++) {
+        primaryDiagonal.push({
+            row: i, //ok
+            column: squareMatrixSize - 1 - i
+        })
+    }
+
+    return primaryDiagonal
+}
+
+
+const test = [
+    [11, 2, 4],
+    [4, 5, 6],
+    [10, 8, -12]
+]
+
+console.clear()
+console.log(diagonalDifference(test))
--- Week-02/FlippingBits.ts
+++ Week-02/FlippingBits.ts
@@ -6,7 +6,7 @@ const flippingBits = (n: number): number => {
     let binary = convertNumberToBinary(n, 32)
     let invertedBinary = ''
     for (let index = 0; index < binary.length; index++) {
-        const letter = binary[index];
+        const letter = binary[index]
         letter === '1' ? invertedBinary += '0' : invertedBinary += '1'
     }
 
