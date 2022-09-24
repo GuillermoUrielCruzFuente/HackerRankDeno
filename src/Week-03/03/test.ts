560ae12be6cbd5e03ebf8e4bf0cf7d4c346faec1
Guillermo Uriel Cruz Fuente <guillermo.951113@gmail.com>
2022-09-23 23:55:34 -0500

    XORStrings test modified to meet HackerRank specs

--- src/Week-03/03/test.ts
+++ src/Week-03/03/test.ts
@@ -1,17 +1,18 @@
-import XORStrings, { Bit } from "./XORStrings.ts";
+import XORStrings, { Bit } from './XORStrings.ts'
 
 type TestCase = {
-	A: Array<Bit>;
-	B: Array<Bit>;
-};
+	A: Array<Bit>
+	B: Array<Bit>
+}
 
 const testCases: Array<TestCase> = [
 	{
-		A: ["1", "0", "1", "0", "1"],
-		B: ["0", "0", "1", "0", "1"],
+		A: ['1', '0', '1', '0', '1'],
+		B: ['0', '0', '1', '0', '1'],
 	},
-];
+]
 
 testCases.forEach((test: TestCase) => {
-	console.log(XORStrings(test.A, test.B));
-});
+	const rawData = XORStrings(test.A, test.B)
+	console.log(rawData.join(''))
+})
