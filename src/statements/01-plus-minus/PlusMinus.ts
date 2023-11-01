12f04ae7a45d8d10b196be907242468f45018026
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-10-31 22:05:33 -0600

    feat: add PlusMinus function

---
+++ src/statements/01-plus-minus/PlusMinus.ts
@@ -0,0 +1,23 @@
+/**
+ * positive, negative, zero
+ */
+type RatioFormattedTuple = [string, string, string];
+
+export const plusMinus = (array: number[]): RatioFormattedTuple => {
+	const { length } = array;
+	const zero = "0.000000";
+
+	if (length === 0) return [zero, zero, zero];
+
+	const ratio: [number, number, number] = [0, 0, 0];
+
+	array.forEach((value) => {
+		if (value > 0) ratio[0]++;
+		else if (value < 0) ratio[1]++;
+		else if (value === 0) ratio[2]++;
+	});
+
+	const finalRatio = ratio.map((rate) => (rate / length).toFixed(6));
+
+	return finalRatio as RatioFormattedTuple;
+};
