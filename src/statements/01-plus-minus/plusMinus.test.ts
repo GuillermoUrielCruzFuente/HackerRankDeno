2cc0f9428245d47cc116a3f16e8108264fd9af25
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-11-09 18:16:24 -0600

    chore: rename plusMinus test file

---
+++ src/statements/01-plus-minus/plusMinus.test.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { plusMinus, type RatioFormattedTuple } from "./plusMinus.ts";
+import { testDataParser } from "../../utils/transformRawData.ts";
+import plusMinusData from "./plus-minus.data.json" with { type: "json" };
+
+Deno.test("#plusMinus", () => {
+	const {
+		inputsToCompute,
+		expectedResults,
+	} = testDataParser<number[], RatioFormattedTuple>(plusMinusData);
+
+	const computedResults = inputsToCompute.map((input, i) => {
+		const result = plusMinus(input);
+		const expected = expectedResults[i];
+
+		console.log({ input, result, expected });
+
+		return result;
+	});
+
+	assertEquals(computedResults, expectedResults);
+});
