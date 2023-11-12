b01756f50e9a01f962bef4fea8ac4230d9b52dbe
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-11-12 17:55:16 -0600

    feat(miniMaxSum): add new implementation

--- src/statements/02-mini-max-sum/miniMaxSum.ts
+++ src/statements/02-mini-max-sum/miniMaxSum.ts
@@ -1,7 +1,18 @@
 export type MinMaxTuple = [number, number];
 export type DataTuple = [number, number, number, number, number];
 
-export const miniMaxSum = (array: DataTuple): MinMaxTuple => {
+export const miniMaxSum = (array: number[]): MinMaxTuple => {
+	// ascendent -> from min to max
+	const sortedArray = array.sort((a, b) => a - b);
+
+	const min = sortedArray.shift()!;
+	const max = sortedArray.pop()!;
+	const restSum = sortedArray.reduce((prev, curr) => prev + curr, 0);
+
+	return [restSum + min, restSum + max];
+};
+
+export const miniMaxSum2 = (array: number[]): MinMaxTuple => {
 	// ascendent -> from min to max
 	const sortedArr = array.sort((a, b) => a - b);
 
