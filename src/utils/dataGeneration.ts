9e6aacefa4be964cd211f5e1fb57e324317a018a
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-11-17 22:22:23 -0600

    docs: improve data generation jsDocs

--- src/utils/dataGeneration.ts
+++ src/utils/dataGeneration.ts
@@ -1,12 +1,13 @@
 /**
- * @param max
- * @returns
+ * @param max the maximum possible value
+ * @returns random integer between 0 and max value
  */
 export const getRandomInt = (max = 100) => Math.ceil(Math.random() * max);
 
 /**
- * @param length final random length
+ * @param length final random array length
  * @returns an array of specified length
+ * filled with random values between 1 and 1000
  */
 export const createRandomArray = (length = 1000) => {
 	const array: number[] = [];
