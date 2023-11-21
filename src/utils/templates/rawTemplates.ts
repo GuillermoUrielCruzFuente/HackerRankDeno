528a6a398f42acb6bab8aeb7a42033c782bfc357
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-11-20 18:52:06 -0600

    feat: add default rawTemplates definition

---
+++ src/utils/templates/rawTemplates.ts
@@ -0,0 +1,64 @@
+import { NameAdapter } from "utils/NameAdapter.ts";
+
+export interface RawTemplates {
+	generateJsonDataTemplate: () => string;
+	generateMDTemplate: (name: NameAdapter) => string;
+	generateTestTemplate: (name: NameAdapter) => string;
+	generateFuncTemplate: (name: NameAdapter) => string;
+}
+
+/**
+ * @returns testing data bundle string template
+ */
+const generateJsonDataTemplate = () => (`
+{
+	"data": [
+		{
+			"input": "",
+			"output": ""
+		}
+	]
+}
+`.trimStart());
+
+/**
+ * @returns statement description string template
+ */
+const generateMDTemplate = (name: NameAdapter) => (`
+# ${name.capitalizedSentence}
+`.trimStart());
+
+/**
+ * @returns testing string template
+ */
+const generateTestTemplate = (name: NameAdapter) => (`
+import { assertEquals } from "std/assert/mod.ts";
+import { testDataParser } from "utils/transformRawData.ts";
+import testingBundle from "./${name.kebabCase}.data.json" with { type: "json" };
+import { ${name.camelCase} } from "./${name.camelCase}.ts";
+
+Deno.test("#${name.camelCase}", () => {
+	const {
+		inputsToCompute,
+		expectedResults,
+	} = testDataParser<T, G>(testingBundle);
+
+	const computedResults = inputsToCompute.map((input) => ${name.camelCase}(input));
+
+	assertEquals(computedResults, expectedResults);
+});
+`.trimStart());
+
+/**
+ * @returns main function string template
+ */
+const generateFuncTemplate = (name: NameAdapter) => (`
+export const ${name.camelCase} = () => {};
+`.trimStart());
+
+export const defaultRawTemplates: RawTemplates = {
+	generateJsonDataTemplate,
+	generateFuncTemplate,
+	generateMDTemplate,
+	generateTestTemplate,
+};
