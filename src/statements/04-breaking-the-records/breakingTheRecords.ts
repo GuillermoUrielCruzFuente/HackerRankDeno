20177987ade2805b9effaba25570347939fcbafc
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-11-24 23:28:46 -0600

    feat(breaking-the-records): add required function structure

--- src/statements/04-breaking-the-records/breakingTheRecords.ts
+++ src/statements/04-breaking-the-records/breakingTheRecords.ts
@@ -1,1 +1,14 @@
-export const breakingTheRecords = () => {};
+/**
+ * An array with the numbers of times she broke her records.
+ *
+ * Index 0 is for breaking most points records,
+ * and index 1 is for breaking least points records.
+ */
+export type MinMaxRecordTuple = [number, number];
+
+export const breakingTheRecords = (scores: number[]): MinMaxRecordTuple => {
+	let max = 0;
+	let min = 0;
+
+	return [max, min];
+};
