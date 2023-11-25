a580c565282db87be110d3f6a89c3d52538aaf72
GuillermoUrielCruzFuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-11-25 15:40:07 -0600

    feat(breaking-the-records): add main function

--- src/statements/04-breaking-the-records/breakingTheRecords.ts
+++ src/statements/04-breaking-the-records/breakingTheRecords.ts
@@ -9,6 +9,20 @@ export type MinMaxRecordTuple = [number, number];
 export const breakingTheRecords = (scores: number[]): MinMaxRecordTuple => {
 	let max = 0;
 	let min = 0;
+	let currentMin = scores[0];
+	let currentMax = scores[0];
+
+	for (const currentScore of scores) {
+		if (currentScore < currentMin) {
+			currentMin = currentScore;
+			min++;
+		}
+
+		if (currentScore > currentMax) {
+			currentMax = currentScore;
+			max++;
+		}
+	}
 
 	return [max, min];
 };
