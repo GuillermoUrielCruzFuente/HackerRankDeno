304e7b0413bf867f643dd4917b7b8110449fc238
GuillermoUrielCruzFuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-11-27 21:46:43 -0600

    chore(camel-case-4): add scaffold

---
+++ src/statements/05-camel-case-4/camelCase4.test.ts
@@ -0,0 +1,15 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { testDataParser } from "utils/transformRawData.ts";
+import testingBundle from "./camel-case-4.data.json" with { type: "json" };
+import { camelCase4 } from "./camelCase4.ts";
+
+Deno.test("#camelCase4", () => {
+	const {
+		inputsToCompute,
+		expectedResults,
+	} = testDataParser<T, G>(testingBundle);
+
+	const computedResults = inputsToCompute.map((input) => camelCase4(input));
+
+	assertEquals(computedResults, expectedResults);
+});
