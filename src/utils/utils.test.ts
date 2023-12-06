95f31c7627a7300a72e181022e0d340a39b2a93d
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-12-05 21:04:48 -0600

    chore: move utils test file to its own folder

--- src/utils/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { assertEquals } from "std/assert/mod.ts";
-import { createRandomArray } from "utils/dataGeneration.ts";
-
-Deno.test("#createRandomArray", () => {
-	const expectedLength = 10000;
-	const array = createRandomArray(expectedLength);
-
-	assertEquals(array.length, expectedLength);
-});
