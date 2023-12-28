ddc040088cdfc670563dbf1b5506ae81a861402e
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-12-27 23:44:29 -0600

    test: add TestDataAdapterTest

---
+++ src/utils/TestDataAdapter/TestDataAdapter.test.ts
@@ -0,0 +1,23 @@
+import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
+
+Deno.test("TestDataAdapter", () => {
+	const testingDataBundle = {
+		data: [
+			{
+				input: "5 6 3 4 4 6 3 3 5 6 3 4 4 6 3 3 4 5 3 5 8",
+				output: "1 2 4",
+			},
+			{
+				input: "6 3 6 6 7 2 1 1 5 6 3 4 4 6 3 3 9 0 6 2 6",
+				output: "1 2 3",
+			},
+		],
+	};
+
+	const { inputsToCompute, expectedResults } = new TestDataAdapter(testingDataBundle, {
+		inputType: "compact-number",
+		outputType: "compact-string",
+	}).parseData<number[], string[]>();
+
+	console.log({ inputsToCompute, expectedResults });
+});
