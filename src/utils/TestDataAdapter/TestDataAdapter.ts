899f026294128db200af5588e82e6aa16cb236fc
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2023-12-31 21:14:43 -0600

    feat: add TestDataAdapter class

--- src/utils/TestDataAdapter/TestDataAdapter.ts
+++ src/utils/TestDataAdapter/TestDataAdapter.ts
@@ -3,3 +3,9 @@ type CompactNumber = "compact-number";
 type Ready = "ready";
 
 type DataSetType = CompactString | CompactNumber | Ready;
+
+export class TestDataAdapter {
+	private readonly inputType: DataSetType;
+	private readonly outputType: DataSetType;
+	private readonly data: unknown[];
+}
