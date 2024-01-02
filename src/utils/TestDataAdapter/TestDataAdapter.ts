f856a768d5f523c463e6085d4dde8c909a46d507
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-01 23:14:47 -0600

    feat: add TestDataParser types

--- src/utils/TestDataAdapter/TestDataAdapter.ts
+++ src/utils/TestDataAdapter/TestDataAdapter.ts
@@ -4,6 +4,15 @@ type Ready = "ready";
 
 type DataSetType = CompactString | CompactNumber | Ready;
 
+type DataConfig = {
+	inputType: DataSetType;
+	outputType: DataSetType;
+};
+
+type Data = {
+	data: unknown[];
+};
+
 export class TestDataAdapter {
 	private readonly inputType: DataSetType;
 	private readonly outputType: DataSetType;
