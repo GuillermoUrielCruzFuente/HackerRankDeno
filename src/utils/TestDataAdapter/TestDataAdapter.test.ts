0dc3293f8530529a5f0e95d6e7eaea3289e6f826
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-04 19:10:31 -0600

    test: add TestDataAdapter outputs and inputs test

--- src/utils/TestDataAdapter/TestDataAdapter.test.ts
+++ src/utils/TestDataAdapter/TestDataAdapter.test.ts
@@ -2,24 +2,18 @@ import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
 import testData from "./TestDataAdapter.data.json" with { type: "json" };
 import { assertEquals } from "std/assert/mod.ts";
 
-Deno.test("TestDataAdapter", () => {
-	const testingDataBundle = {
-		data: [
-			{
-				input: "5 6 3 4 4 6 3 3 5 6 3 4 4 6 3 3 4 5 3 5 8",
-				output: "1 2 4",
-			},
-			{
-				input: "6 3 6 6 7 2 1 1 5 6 3 4 4 6 3 3 9 0 6 2 6",
-				output: "1 2 3",
-			},
-		],
-	};
+Deno.test("#getInputs", () => {
+	const testAdapter = new TestDataAdapter(testData);
 
-	const { inputsToCompute, expectedResults } = new TestDataAdapter(testingDataBundle, {
-		inputType: "compact-number",
-		outputType: "compact-string",
-	}).parseData<number[], string[]>();
+	const inputs = testAdapter.getInputs("compact-number");
 
-	console.log({ inputsToCompute, expectedResults });
+	assertEquals(inputs, [[1, 2, 3, 4, 5, 6, 6, 4]]);
+});
+
+Deno.test("#getOutputs", () => {
+	const testAdapter = new TestDataAdapter(testData);
+
+	const outputs = testAdapter.getExpectedResults("compact-number");
+
+	assertEquals(outputs, [[1, 4, 2, 5, 0, 8, 9, 9]]);
 });
