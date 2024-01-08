3b616587e05555843f5c7def18a7a997c4b64021
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-07 19:39:53 -0600

    test(breaking-the-records): add new TestDataAdapter

--- src/statements/04-breaking-the-records/breakingTheRecords.test.ts
+++ src/statements/04-breaking-the-records/breakingTheRecords.test.ts
@@ -1,15 +1,14 @@
 import { assertEquals } from "std/assert/mod.ts";
-import { testDataParser } from "utils/transformRawData.ts";
 import testingBundle from "./breaking-the-records.data.json" with { type: "json" };
-import { breakingTheRecords, type MinMaxRecordTuple } from "./breakingTheRecords.ts";
+import { breakingTheRecords } from "./breakingTheRecords.ts";
+import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
 
 Deno.test("#breakingTheRecords", () => {
-	const {
-		inputsToCompute,
-		expectedResults,
-	} = testDataParser<number[], MinMaxRecordTuple>(testingBundle);
+	const dataAdapter = new TestDataAdapter(testingBundle);
 
-	const computedResults = inputsToCompute.map((input) => breakingTheRecords(input));
+	const computedResults = dataAdapter
+		.getInputs("compact-number")
+		.map((input) => breakingTheRecords(input));
 
-	assertEquals(computedResults, expectedResults);
+	assertEquals(computedResults, dataAdapter.getExpectedResults("compact-number"));
 });
