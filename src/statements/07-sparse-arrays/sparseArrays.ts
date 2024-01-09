b777095f1c7c427945dc6e79c3faf1d166a3d5e3
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-09 14:39:38 -0600

    feat(sparse-arrays): add sparseArrays first implementation

--- src/statements/07-sparse-arrays/sparseArrays.ts
+++ src/statements/07-sparse-arrays/sparseArrays.ts
@@ -1,1 +1,23 @@
-export const sparseArrays = () => {};
+type ConcurrencyTableItem = {
+	[key: string]: number;
+};
+
+const repetitionsInStringArray = (wordArray: string[]): ConcurrencyTableItem => {
+	const dictionary: ConcurrencyTableItem = {};
+
+	wordArray.forEach((word: string) => {
+		word in dictionary ? dictionary[word]++ : dictionary[word] = 1;
+	});
+
+	return dictionary;
+};
+
+type SparseArraysArgs = {
+	strings: string[];
+	queries: string[];
+};
+
+export const sparseArrays = ({ strings, queries }: SparseArraysArgs) => {
+	const repetitions = repetitionsInStringArray(strings);
+	return queries.map((query) => repetitions[query] ?? 0);
+};
