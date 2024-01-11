b9e9f426ba8fce9a92b73b31675e6cc8a44be00f
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-11 14:53:05 -0600

    test(camel-case-4): add new TestDataAdapter

--- src/statements/05-camel-case-4/camelCase4.test.ts
+++ src/statements/05-camel-case-4/camelCase4.test.ts
@@ -1,18 +1,14 @@
 import { assertEquals } from "std/assert/mod.ts";
-import { testDataParser } from "utils/transformRawData.ts";
 import testingBundle from "./camel-case-4.data.json" with { type: "json" };
 import { camelCase4 } from "./camelCase4.ts";
+import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
 
 Deno.test("#camelCase4", () => {
-	const {
-		inputsToCompute,
-		expectedResults,
-	} = testDataParser<string, string>(testingBundle, {
-		inputAsArray: false,
-		outputAsArray: false,
-	});
+	const adapter = new TestDataAdapter(testingBundle);
 
-	const computedResults = inputsToCompute.map((input) => camelCase4(input));
+	const computedResults = adapter
+		.getInputs<string>("ready")
+		.map((input) => camelCase4(input));
 
-	assertEquals(computedResults, expectedResults);
+	assertEquals(computedResults, adapter.getExpectedResults("ready"));
 });
