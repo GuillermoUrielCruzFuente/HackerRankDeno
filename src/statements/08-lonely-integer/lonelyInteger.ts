71915a59f198f6e0d2f607d40065c64ba5006979
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-14 01:02:29 -0600

    feat(lonely-integer): add first implementation

--- src/statements/08-lonely-integer/lonelyInteger.ts
+++ src/statements/08-lonely-integer/lonelyInteger.ts
@@ -1,1 +1,25 @@
-export const lonelyInteger = () => {};
+type ConcurrencyTableItem = {
+	[key: number]: number;
+};
+
+const repetitionsInNumericArray = (numbers: number[]): ConcurrencyTableItem => {
+	const numberReps: ConcurrencyTableItem = {};
+
+	numbers.forEach((n: number) => {
+		n in numberReps ? numberReps[n]++ : numberReps[n] = 1;
+	});
+
+	return numberReps;
+};
+
+export const lonelyInteger = (integers: number[]): number => {
+	const concurrencyTable = repetitionsInNumericArray(integers);
+
+	for (const int of integers) {
+		if (concurrencyTable[int] === 1) {
+			return int;
+		}
+	}
+
+	throw new Error("There is no unique element in this array");
+};
