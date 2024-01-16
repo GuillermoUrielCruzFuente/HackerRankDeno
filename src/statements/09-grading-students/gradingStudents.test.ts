3c6f06677876316d056ab1bf2d5be40a1d979120
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-15 22:37:54 -0600

    chore(grading-students): add initial scaffold

---
+++ src/statements/09-grading-students/gradingStudents.test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
+import testingBundle from "./grading-students.data.json" with { type: "json" };
+import { gradingStudents } from "./gradingStudents.ts";
+
+Deno.test("#gradingStudents", () => {
+	const adapter = new TestDataAdapter(testingBundle);
+
+	const computedResults = adapter
+		.getInputs("")
+		.map((input) => gradingStudents(input));
+
+	assertEquals(computedResults, adapter.getExpectedResults(""));
+});
