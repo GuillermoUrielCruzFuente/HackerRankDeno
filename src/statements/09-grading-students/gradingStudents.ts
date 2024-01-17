3a51d5dc5d462243a5ff55c963e4694a84b58d49
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-17 15:28:53 -0600

    feat(grading-students): add implementation

--- src/statements/09-grading-students/gradingStudents.ts
+++ src/statements/09-grading-students/gradingStudents.ts
@@ -1,1 +1,19 @@
-export const gradingStudents = () => {};
+const computeNextMultipleOf5 = (n: number) => {
+	while (n % 5 != 0) {
+		n++;
+	}
+
+	return n;
+};
+
+export const gradingStudents = (grades: number[]): number[] => {
+	return grades.map((grade) => {
+		if (grade < 38) return grade;
+
+		const nextMultiple = computeNextMultipleOf5(grade);
+
+		if (nextMultiple - grade < 3) return nextMultiple;
+
+		return grade;
+	});
+};
