bdff2c7c8ade15267332268a629e518835a214d3
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-19 23:43:10 -0600

    chore(diagonal-difference): add initial scaffold

---
+++ src/statements/11-diagonal-difference/diagonalDifference.test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
+import testingBundle from "./diagonal-difference.data.json" with { type: "json" };
+import { diagonalDifference } from "./diagonalDifference.ts";
+
+Deno.test("#diagonalDifference", () => {
+	const adapter = new TestDataAdapter(testingBundle);
+
+	const computedResults = adapter
+		.getInputs("")
+		.map((input) => diagonalDifference(input));
+
+	assertEquals(computedResults, adapter.getExpectedResults(""));
+});
