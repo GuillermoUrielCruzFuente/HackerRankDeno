2b26b025744b79515b682b222fdd016c56dd945f
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-20 23:49:04 -0600

    feat(flipping-bits): add main function

--- src/statements/10-flipping-bits/flippingBits.ts
+++ src/statements/10-flipping-bits/flippingBits.ts
@@ -1,1 +1,11 @@
-export const flippingBits = () => {};
+export const flippingBits = (n: number): number => {
+	const binary = convertNumberToBinary(n, 32);
+
+	let invertedBinary = "";
+	for (let index = 0; index < binary.length; index++) {
+		const letter = binary[index];
+		letter === "1" ? invertedBinary += "0" : invertedBinary += "1";
+	}
+
+	return convertBinaryToNumber(invertedBinary);
+};
