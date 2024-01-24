49158cd56f0ca216ae992c5d8144fe99424e2eff
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-23 23:47:15 -0600

    feat(flipping-bits): add other number to binary converter

--- src/statements/10-flipping-bits/flippingBits.ts
+++ src/statements/10-flipping-bits/flippingBits.ts
@@ -30,3 +30,17 @@ export const convertNumberToBinary = (n: number, bits = 32): string => {
 
 	return missingPart + binary;
 };
+
+/**
+ * @see {@link https://stackoverflow.com/a/24153275}
+ */
+export const createBinaryString = (nMask: number) => {
+	// nMask must be between -2147483648 and 2147483647
+	for (
+		var nFlag = 0, nShifted = nMask, sMask = "";
+		nFlag < 32;
+		nFlag++, sMask += String(nShifted >>> 31), nShifted <<= 1
+	);
+
+	return sMask;
+};
