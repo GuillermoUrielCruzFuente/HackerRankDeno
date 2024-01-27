1ec5984843b7ecdf1c313e622d5c9d620fbc4b80
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-26 22:12:19 -0600

    test(diagonal-difference): add testing data types

--- src/statements/11-diagonal-difference/diagonalDifference.test.ts
+++ src/statements/11-diagonal-difference/diagonalDifference.test.ts
@@ -7,8 +7,8 @@ Deno.test("#diagonalDifference", () => {
 	const adapter = new TestDataAdapter(testingBundle);
 
 	const computedResults = adapter
-		.getInputs("")
+		.getInputs<number[][]>("ready")
 		.map((input) => diagonalDifference(input));
 
-	assertEquals(computedResults, adapter.getExpectedResults(""));
+	assertEquals(computedResults, adapter.getExpectedResults("ready"));
 });
