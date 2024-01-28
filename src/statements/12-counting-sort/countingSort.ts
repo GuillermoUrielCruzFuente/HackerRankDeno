765a8c9c193abc8cbf3fee723d0a3687b145ba58
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-27 23:16:39 -0600

    feat(counting-sort): add first implementation

--- src/statements/12-counting-sort/countingSort.ts
+++ src/statements/12-counting-sort/countingSort.ts
@@ -1,1 +1,14 @@
-export const countingSort = () => {};
+/**
+ * @param arr array with random values
+ * @returns array of frecuencies
+ */
+export const countingSort = (arr: number[]): number[] => {
+	const frequencies = new Array<number>(100);
+	frequencies.fill(0);
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] < 100) frequencies[arr[i]]++;
+	}
+
+	return frequencies;
+};
