4f6c39544d336d33de06ae6ecd27a74908a8aa04
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-01-29 23:03:36 -0600

    feat(diagonal-difference): add initial impl

--- src/statements/11-diagonal-difference/diagonalDifference.ts
+++ src/statements/11-diagonal-difference/diagonalDifference.ts
@@ -1,1 +1,15 @@
-export const diagonalDifference = () => {};
+/**
+ * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+ */
+export const diagonalDifference = (matrix: number[][]) => {
+	const size = matrix[0].length;
+	let primarySum = 0;
+	let secondarySum = 0;
+
+	for (let i = 0; i < size; i++) {
+		primarySum += matrix[i][i];
+		secondarySum += matrix[i][size - 1 - i];
+	}
+
+	return Math.abs(primarySum - secondarySum);
+};
