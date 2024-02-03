5954c7e131295ee0f88fdf2c86c73f8d78c1a261
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-02-02 22:44:03 -0600

    chore(counting-valleys): add initial scaffold

---
+++ src/statements/13-counting-valleys/countingValleys.test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
+import testingBundle from "./counting-valleys.data.json" with { type: "json" };
+import { countingValleys } from "./countingValleys.ts";
+
+Deno.test("#countingValleys", () => {
+	const adapter = new TestDataAdapter(testingBundle);
+
+	const computedResults = adapter
+		.getInputs("")
+		.map((input) => countingValleys(input));
+
+	assertEquals(computedResults, adapter.getExpectedResults(""));
+});
