45b59ea8c1178a7b67b21fbc1a138e7d813bad21
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-02-04 22:40:38 -0600

    feat(counting-valleys): add initial implementation

--- src/statements/13-counting-valleys/countingValleys.ts
+++ src/statements/13-counting-valleys/countingValleys.ts
@@ -1,1 +1,32 @@
-export const countingValleys = () => {};
+export const countingValleys = (stepsRegister: string): number => {
+	// hikes always start at sea level
+	let currentAltitude = 0;
+	let counterValleys = 0;
+	let hasEnteredAValley = false;
+
+	for (let stepNumber = 0; stepNumber < stepsRegister.length; stepNumber++) {
+		const step = stepsRegister[stepNumber];
+
+		switch (step) {
+			case "D":
+				currentAltitude--;
+				break;
+			case "U":
+				currentAltitude++;
+				break;
+			default:
+				throw new Error(`[${stepNumber}]: ${step}, is not a valid step chatacter`);
+		}
+
+		if (currentAltitude === -1) {
+			hasEnteredAValley = true;
+		}
+
+		if (currentAltitude === 0 && hasEnteredAValley) {
+			hasEnteredAValley = false;
+			counterValleys++;
+		}
+	}
+
+	return counterValleys;
+};
