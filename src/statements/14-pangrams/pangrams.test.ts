5ab63ad284615105af2113b6be422141fbdd538c
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-02-05 20:50:44 -0600

    chore(pangrams): add initial scaffold

---
+++ src/statements/14-pangrams/pangrams.test.ts
@@ -0,0 +1,14 @@
+import { assertEquals } from "std/assert/mod.ts";
+import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
+import testingBundle from "./pangrams.data.json" with { type: "json" };
+import { pangrams } from "./pangrams.ts";
+
+Deno.test("#pangrams", () => {
+	const adapter = new TestDataAdapter(testingBundle);
+
+	const computedResults = adapter
+		.getInputs("")
+		.map((input) => pangrams(input));
+
+	assertEquals(computedResults, adapter.getExpectedResults(""));
+});
