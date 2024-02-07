629cfb38094eae034184d9cb4f28db914b3ed308
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-02-06 23:34:27 -0600

    feat(pangrams): add implementation

--- src/statements/14-pangrams/pangrams.ts
+++ src/statements/14-pangrams/pangrams.ts
@@ -1,1 +1,28 @@
-export const pangrams = () => {};
+type TextType = "pangram" | "not pangram";
+type TableItem = {
+	[key: string]: boolean;
+};
+
+const TOTAL_ALPHABET_LETTERS = 26;
+
+const letterValidator = /[a-z]/;
+
+export const pangrams = (text: string): TextType => {
+	let newLetterCounter = 0;
+	text = text.toLowerCase();
+
+	const dictionary: TableItem = {};
+
+	for (let i = 0; i < text.length; i++) {
+		const letter = text[i];
+
+		if (!letterValidator.test(letter)) continue;
+
+		if (!(letter in dictionary)) {
+			newLetterCounter++;
+			dictionary[letter] = true;
+		}
+	}
+
+	return newLetterCounter === TOTAL_ALPHABET_LETTERS ? "pangram" : "not pangram";
+};
