d65dfd9e28866c85d21a84fa13c2a35f8515cf10
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-02-08 22:18:54 -0600

    feat(mars-exploration): add initial implementation

--- src/statements/15-mars-exploration/marsExploration.ts
+++ src/statements/15-mars-exploration/marsExploration.ts
@@ -1,1 +1,20 @@
-export const marsExploration = () => {};
+export const marsExploration = (signal: string): number => {
+	let totalCorruptions = 0;
+
+	for (let i = 0; i < signal.length; i = i + 3) {
+		const sequence = signal.slice(i, i + 3);
+		totalCorruptions += getCorruptionsInSequence(sequence);
+	}
+
+	return totalCorruptions;
+};
+
+const getCorruptionsInSequence = (corruptedSignal: string) => {
+	let counter = 0;
+
+	corruptedSignal[0] != "S" && counter++;
+	corruptedSignal[1] != "O" && counter++;
+	corruptedSignal[2] != "S" && counter++;
+
+	return counter;
+};
