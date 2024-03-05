8c9c37589463e7406421f294505e97473524efd5
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-04 19:19:17 -0600

    test(permuting-two-arrays): add testing data types

--- src/statements/16-permuting-two-arrays/permutingTwoArrays.test.ts
+++ src/statements/16-permuting-two-arrays/permutingTwoArrays.test.ts
@@ -1,14 +1,14 @@
 import { assertEquals } from "std/assert/mod.ts";
 import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
 import testingBundle from "./permuting-two-arrays.data.json" with { type: "json" };
-import { permutingTwoArrays } from "./permutingTwoArrays.ts";
+import { type CanPermute, type PermutingParams, permutingTwoArrays } from "./permutingTwoArrays.ts";
 
 Deno.test("#permutingTwoArrays", () => {
 	const adapter = new TestDataAdapter(testingBundle);
 
 	const computedResults = adapter
-		.getInputs("")
+		.getInputs<PermutingParams>("ready")
 		.map((input) => permutingTwoArrays(input));
 
-	assertEquals(computedResults, adapter.getExpectedResults(""));
+	assertEquals(computedResults, adapter.getExpectedResults<CanPermute>("ready"));
 });
