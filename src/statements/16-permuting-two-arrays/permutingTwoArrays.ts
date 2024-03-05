d25b9c59427459eac7ba91df65783a9bde111b05
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-04 19:20:01 -0600

    feat(permuting-two-arrays): add functions params and return types

--- src/statements/16-permuting-two-arrays/permutingTwoArrays.ts
+++ src/statements/16-permuting-two-arrays/permutingTwoArrays.ts
@@ -1,1 +1,11 @@
-export const permutingTwoArrays = () => {};
+export type PermutingParams = {
+	k: number;
+	a: number[];
+	b: number[];
+};
+
+export type CanPermute = "YES" | "NO";
+
+export const permutingTwoArrays = ({ k, a, b }: PermutingParams): CanPermute => {
+	return "NO";
+};
