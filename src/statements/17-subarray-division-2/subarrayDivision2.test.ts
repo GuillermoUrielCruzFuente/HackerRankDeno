c333dc25d460affddc99eb4fb82af4b9598263fc
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-05 21:43:02 -0600

    test(subarray-division-2): add testing data types

--- src/statements/17-subarray-division-2/subarrayDivision2.test.ts
+++ src/statements/17-subarray-division-2/subarrayDivision2.test.ts
@@ -1,14 +1,14 @@
 import { assertEquals } from "std/assert/mod.ts";
 import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
 import testingBundle from "./subarray-division-2.data.json" with { type: "json" };
-import { subarrayDivision2 } from "./subarrayDivision2.ts";
+import { subarrayDivision2, type SubarrayDivision2Params } from "./subarrayDivision2.ts";
 
 Deno.test("#subarrayDivision2", () => {
 	const adapter = new TestDataAdapter(testingBundle);
 
 	const computedResults = adapter
-		.getInputs("")
+		.getInputs<SubarrayDivision2Params>("ready")
 		.map((input) => subarrayDivision2(input));
 
-	assertEquals(computedResults, adapter.getExpectedResults(""));
+	assertEquals(computedResults, adapter.getExpectedResults<number>("ready"));
 });
