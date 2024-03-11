5412243e9a043835d02b046886e8a10485ae0f13
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-10 22:12:47 -0600

    feat(subarray-division-2): add initial implementation

--- src/statements/17-subarray-division-2/subarrayDivision2.ts
+++ src/statements/17-subarray-division-2/subarrayDivision2.ts
@@ -1,1 +1,30 @@
-export const subarrayDivision2 = () => {};
+export type SubarrayDivision2Params = {
+	chocolateBar: Array<number>;
+	birthDay: number;
+	birthMonth: number;
+};
+
+export const subarrayDivision2 = (
+	{ chocolateBar, birthDay, birthMonth }: SubarrayDivision2Params,
+) => {
+	let numberOfPosibleSlices = 0;
+
+	for (let i = 0; i < chocolateBar.length; i++) {
+		const sliceStep = i + birthMonth - 1;
+		const thereIsEnoughChocolate = sliceStep <= chocolateBar.length - 1;
+
+		if (thereIsEnoughChocolate) {
+			let chocolateSquaresAccumulator = 0;
+
+			for (let j = i; j <= sliceStep; j++) {
+				chocolateSquaresAccumulator += chocolateBar[j];
+			}
+
+			numberOfPosibleSlices += chocolateSquaresAccumulator === birthDay ? 1 : 0;
+		} else {
+			break;
+		}
+	}
+
+	return numberOfPosibleSlices;
+};
