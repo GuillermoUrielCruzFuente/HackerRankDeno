2ee72250ea04d22f3cf5af9bfa81237f470ad9f7
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-12 16:07:51 -0600

    test(xor-strings-3): add testing data types

--- src/statements/18-xor-strings-3/xorStrings3.test.ts
+++ src/statements/18-xor-strings-3/xorStrings3.test.ts
@@ -1,14 +1,14 @@
 import { assertEquals } from "std/assert/mod.ts";
 import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
 import testingBundle from "./xor-strings-3.data.json" with { type: "json" };
-import { xorStrings3 } from "./xorStrings3.ts";
+import { type Params, xorStrings3 } from "./xorStrings3.ts";
 
 Deno.test("#xorStrings3", () => {
 	const adapter = new TestDataAdapter(testingBundle);
 
 	const computedResults = adapter
-		.getInputs("")
+		.getInputs<Params>("ready")
 		.map((input) => xorStrings3(input));
 
-	assertEquals(computedResults, adapter.getExpectedResults(""));
+	assertEquals(computedResults, adapter.getExpectedResults("ready"));
 });
