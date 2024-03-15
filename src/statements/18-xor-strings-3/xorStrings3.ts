c2fb3b4d5af44382b6b09d2eaf8db42371845b4a
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-14 21:21:26 -0600

    feat(xor-strings-3): add impl

--- src/statements/18-xor-strings-3/xorStrings3.ts
+++ src/statements/18-xor-strings-3/xorStrings3.ts
@@ -1,1 +1,15 @@
-export const xorStrings3 = () => {};
+export type Params = { a: string; b: string };
+
+export const xorStrings3 = ({ a, b }: Params) => {
+	//initialize a new array with the same length and fill it with '0'
+	const XOR: string[] = new Array(a.length).fill("0");
+
+	//compare every bit in order to decide the output
+	for (let i = 0; i < a.length; i++) {
+		if (a[i] != b[i]) {
+			XOR[i] = "1";
+		}
+	}
+
+	return XOR.join("");
+};
