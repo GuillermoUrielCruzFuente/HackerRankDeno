9f229a0c8fc74b0605f57743a533c26254e9a00c
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-16 11:29:29 -0600

    feat(utils): remove extra spaces from prompt and add messages

--- src/utils/processString.ts
+++ src/utils/processString.ts
@@ -1,7 +1,19 @@
 import { writeText } from "https://deno.land/x/copy_paste@v1.1.3/mod.ts";
+import { removeExtraSpaces } from "utils/validateName.ts";
 
-const rawString = prompt("raw string: ", "");
+console.clear();
 
-const array = "[" + rawString?.split(" ").join() + "]";
+console.log(
+	`%cNote: repeated spaces will be reduced to only one!`,
+	`color: red; font-weight: bold`,
+);
 
+const rawString = prompt("raw string: ", "")!;
+const stringArray = removeExtraSpaces(rawString).split(" ");
+const array = "[" + stringArray.join() + "]";
 await writeText(array);
+
+console.log(
+	`%c - Array copied to clipboard, final length: ${stringArray.length}`,
+	`color: violet; font-weight: bold`,
+);
