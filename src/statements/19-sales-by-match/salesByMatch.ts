184276bfe251c20b07b7e615701fd855720d77ad
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-19 21:31:10 -0600

    feat(sales-by-match): add two impls

--- src/statements/19-sales-by-match/salesByMatch.ts
+++ src/statements/19-sales-by-match/salesByMatch.ts
@@ -1,3 +1,41 @@
+type Dict = {
+	[key: number]: number;
+};
+
 export const salesByMatch = (socks: number[]): number => {
-	return 0;
+	const socksDict: Dict = {};
+
+	for (let i = 0; i < socks.length; i++) {
+		const sock = socks[i];
+		sock in socksDict ? socksDict[sock] += 1 : socksDict[sock] = 1;
+	}
+
+	const pairs = Object.values(socksDict).reduce((acc, currentValue) => {
+		const isPair = currentValue % 2 === 0;
+		const newPairs = isPair ? currentValue / 2 : (currentValue - 1) / 2;
+		return acc + newPairs;
+	}, 0);
+
+	return pairs;
+};
+
+export const sales = (socks: number[]) => {
+	const { length } = socks;
+	let pairCounter = 0;
+
+	for (let i = 0; i < length; i++) {
+		const sock = socks[i];
+
+		for (let j = i + 1; j < length; j++) {
+			const anotherSock = socks[j];
+
+			if (sock === anotherSock) {
+				pairCounter++;
+				socks[j] = NaN;
+				break;
+			}
+		}
+	}
+
+	return pairCounter;
 };
