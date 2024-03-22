ad1469ccc4fc1fafc6a6c5dcab183d49adfadb9b
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-21 22:07:22 -0600

    feat(migratory-birds): add first impl

--- src/statements/20-migratory-birds/migratoryBirds.ts
+++ src/statements/20-migratory-birds/migratoryBirds.ts
@@ -1,1 +1,36 @@
-export const migratoryBirds = () => {};
+type BirdDictionary = {
+	[key: number]: number;
+};
+
+export const migratoryBirds = (birds: number[]) => {
+	const dict: BirdDictionary = {};
+
+	for (let i = 0; i < birds.length; i++) {
+		const bird = birds[i];
+
+		if (bird in dict) {
+			dict[bird] += 1;
+		} else {
+			dict[bird] = 1;
+		}
+	}
+
+	let maxSightings: number[] = [];
+	let currentMaxSightings = 0;
+
+	for (const [birdID, sightings] of Object.entries(dict)) {
+		if (sightings > currentMaxSightings) {
+			currentMaxSightings = sightings;
+			maxSightings = [];
+			maxSightings.push(+birdID);
+		} else if (sightings === currentMaxSightings) {
+			maxSightings.push(+birdID);
+		}
+	}
+
+	if (maxSightings.length > 1) {
+		return Math.min(...maxSightings);
+	}
+
+	return maxSightings[0];
+};
