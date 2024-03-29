d7466406605f5ceb422043d32d41c8f300d737c1
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-29 14:26:41 -0600

    feat(zig-zag-sequence): add initial impl

--- src/statements/22-zig-zag-sequence/zigZagSequence.ts
+++ src/statements/22-zig-zag-sequence/zigZagSequence.ts
@@ -1,1 +1,24 @@
-export const zigZagSequence = () => {};
+export const zigZagSequence = (array: number[]): number[] => {
+	array.sort((a, b) => a - b);
+
+	const swap = (from: number, to: number) => {
+		[array[from], array[to]] = [array[to], array[from]];
+	};
+
+	const midIndex = Math.trunc((array.length + 1) / 2) - 1;
+	const lastIndex = array.length - 1;
+
+	swap(midIndex, lastIndex);
+
+	let aIndex = midIndex + 1;
+	let bIndex = lastIndex - 1;
+
+	while (aIndex <= bIndex) {
+		swap(aIndex, bIndex);
+
+		aIndex = aIndex + 1;
+		bIndex = bIndex - 1;
+	}
+
+	return array;
+};
