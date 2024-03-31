ad2b2b772f2798d3dd3b0ce9713c225c8b33d067
Guillermo Uriel Cruz Fuente <guillermo.uriel.cruz.fuente@gmail.com>
2024-03-31 17:46:56 -0600

    test(drawing-book): add testing data types

--- src/statements/23-drawing-book/drawingBook.test.ts
+++ src/statements/23-drawing-book/drawingBook.test.ts
@@ -1,14 +1,14 @@
 import { assertEquals } from "std/assert/mod.ts";
 import { TestDataAdapter } from "utils/TestDataAdapter/TestDataAdapter.ts";
 import testingBundle from "./drawing-book.data.json" with { type: "json" };
-import { drawingBook } from "./drawingBook.ts";
+import { drawingBook, drawingBookParams } from "./drawingBook.ts";
 
 Deno.test("#drawingBook", () => {
 	const adapter = new TestDataAdapter(testingBundle);
 
 	const computedResults = adapter
-		.getInputs("")
+		.getInputs<drawingBookParams>("ready")
 		.map((input) => drawingBook(input));
 
-	assertEquals(computedResults, adapter.getExpectedResults(""));
+	assertEquals(computedResults, adapter.getExpectedResults("ready"));
 });
